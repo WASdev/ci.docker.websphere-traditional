ca5a4f48257c93700d4648b630dce95b24b922a6
Kavitha Suresh Kumar <kavisuresh@in.ibm.com>
2016-05-25 19:06:16 +0530

    Base moved to 16.04 , apt-get update added

--- network-deployment/appserver/updateHostAndFederateServer.sh
+++ network-deployment/appserver/updateHostAndFederateServer.sh
@@ -7,73 +7,105 @@
 #                                                                                   #
 #####################################################################################
 
-#Check whether profile name is provided or use default
-if [ "$PROFILE_NAME" = "" ] 
-then
-     PROFILE_NAME="AppSrv01"
-fi
+setEnv()
+{
+     #Check whether profile name is provided or use default
+     if [ "$PROFILE_NAME" = "" ] 
+     then
+          PROFILE_NAME="AppSrv01"
+     fi
 
-#Check whether node name is provided or use default
-if [ "$NODE_NAME" = "" ] 
-then
-     NODE_NAME="ServerNode"
-fi
+     #Check whether node name is provided or use default
+     if [ "$NODE_NAME" = "" ] 
+     then
+          NODE_NAME="ServerNode"
+     fi
 
-#Check whether dmgr host is provided or use default
-if [ "$DMGR_HOST" = "" ]
-then
-     DMGR_HOST="dmgr"
-fi
+     #Check whether dmgr host is provided or use default
+     if [ "$DMGR_HOST" = "" ]
+     then
+          DMGR_HOST="dmgr"
+     fi
 
-#Check whether dmgr port is provided or use default
-if [ "$DMGR_PORT" = "" ]
-then
-     DMGR_PORT="8879"
-fi
+     #Check whether dmgr port is provided or use default
+     if [ "$DMGR_PORT" = "" ]
+     then
+          DMGR_PORT="8879"
+     fi
 
-#Get the container hostname
-host=`hostname`
+     #Get the container hostname
+     host=`hostname`
+}
+
+startNodeAndServer()
+{
+     echo "Starting nodeagent..............."
+     /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startNode.sh
+
+     #if nodeagent started successfully , start the server
+     if [ $? = 0 ]
+     then
+           echo "Starting server......................."
+           /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startServer.sh server1
+     else
+           echo " Nodeagent startup failed , exiting....... "
+           exit 1
+     fi
+}
+
+stopServerAndNode()
+{
+     echo "Stopping server......................."
+     /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/stopServer.sh server1
+
+     if [ $? = 0 ]
+     then
+           echo " Server stopped successfully. "
+     fi
+
+     echo "Stopping nodeagent..............."
+     /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/stopNode.sh
+
+     if [ $? = 0 ]
+     then
+           echo " Nodeagent stopped successfully. "
+     fi
+}
+
+updateHostNameAndAddNode()
+{
+     # Update the hostname
+     /opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -lang jython -conntype NONE -f /work/updateHostName.py \
+     ServerNode $host
+    
+     # Add the node
+     /opt/IBM/WebSphere/AppServer/bin/addNode.sh $DMGR_HOST $DMGR_PORT
+}
+
+renameNode()
+{
+      # rename the node
+      /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/renameNode.sh $DMGR_HOST $DMGR_PORT $NODE_NAME
+}
+
+setEnv
 
 # check if nodeagent already created
 if [ -d /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/logs/nodeagent ]
 then
-        echo "Starting nodeagent..............."
-	/opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startNode.sh
+      startNodeAndServer
 else
-	# Update the hostname
-	/opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -lang jython -conntype NONE -f /work/updateHostName.py \
-        ServerNode $host
-     
-	# Add the node
-	/opt/IBM/WebSphere/AppServer/bin/addNode.sh $DMGR_HOST $DMGR_PORT
-
-        # Rename the node name and start the node
-	if [ $NODE_NAME != "ServerNode" ]
-	then
-	        # rename the node
-        	/opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/renameNode.sh $DMGR_HOST $DMGR_PORT $NODE_NAME
-      
-                echo "Starting nodeagent................"
-	        /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startNode.sh
-        fi
+      updateHostNameAndAddNode
+        
+      # Rename the node name and start the node
+      if [ $NODE_NAME != "ServerNode" ]
+      then
+           renameNode
+           startNodeAndServer
+      fi
 fi
 
-#if nodeagent started successfully , start the server
-if [ $? = 0 ]
-then
-        echo "Starting server......................."
-	/opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startServer.sh server1
-else
-        echo " Nodeagent startup failed , exiting....... "
-	exit 1
-fi
-
-# Exit the container if server startup fails
-if [ $? != 0 ]
-then
-	echo " Server startup failed , exiting.........."
-        exit 1
-fi
+trap "stopServerAndNode" SIGTERM
 
 sleep 10
 
