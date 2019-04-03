7fe7d2d8f187cc85930efe9870c65362c3d8052e
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2019-04-03 13:43:39 -0400

    Script fixes and Dockerfile improvements

--- docker-build/9.0.0.9/scripts/applyConfig.sh
+++ docker-build/9.0.0.9/scripts/applyConfig.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 if [ ! -z "$1" ]; then
-	/opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -conntype None -f /work/applyConfig.py $1
-	exit 0
-fi
-
-if [ -f /work/config/was-config.props ]; then
-	/opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -conntype None -f /work/applyConfig.py /work/config/was-config.props
+        /opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -conntype None -f /work/applyConfig.py $1
+        exit 0
+else 
+      find /work/config -type f -name "*.props" -exec /work/applyConfig.sh {} \;
 fi
