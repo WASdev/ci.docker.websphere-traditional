e6654857bcaf65e46a46d465dd4fcd37809f0ec7
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2019-04-04 17:14:42 -0400

    Use find to look for props and scripts

--- docker-build/9.0.0.9/scripts/applyConfig.sh
+++ docker-build/9.0.0.9/scripts/applyConfig.sh
@@ -3,5 +3,5 @@ if [ ! -z "$1" ]; then
         /opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -conntype None -f /work/applyConfig.py $1
         exit 0
 else 
-      find /work/config -type f -name "*.props" -exec /work/applyConfig.sh {} \;
+      find /work/config -type f -name "*.props"  -print0 | sort -z | xargs -0 -n 1 -r /work/applyConfig.sh
 fi
