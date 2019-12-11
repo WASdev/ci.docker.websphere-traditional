c75186ec5112e7bd38f4940a8047eb61103c1737
crpotter <crpotter@us.ibm.com>
2019-12-11 10:45:40 -0500

    Update copyrights

--- docker-build/9.0.5.2/scripts/apply_fixes.sh
+++ docker-build/9.0.5.2/scripts/apply_fixes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################################################################
-# (C) Copyright IBM Corporation 2018                                       #
+# (C) Copyright IBM Corporation 2019                                       #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
