1cb380c635e70319fdf4e4dd7fff73f8963f5890
Bradley Mayo <Bradley.Mayo@ibm.com>
2022-03-30 23:58:41 -0400

    Added version 8.5.5.21

--- docker-build/8.5.5.18/scripts/applyConfig.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-filename = sys.argv[0]
-AdminTask.applyConfigProperties('[-propertiesFileName ' + filename + ' -validate true]')
-AdminConfig.save()
--- docker-build/8.5.5.18/scripts/configure_logging.sh
+++
@@ -1,5 +0,0 @@
-ENABLE_BASIC_LOGGING="${ENABLE_BASIC_LOGGING:-false}"
-
-if [ "$ENABLE_BASIC_LOGGING" = false ]; then
-	/opt/IBM/WebSphere/AppServer/bin/wsadmin.sh -conntype None -f /work/configHPEL.py
-fi
