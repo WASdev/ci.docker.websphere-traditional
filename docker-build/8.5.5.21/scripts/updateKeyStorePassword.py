2b61b56e6cd00f28f714568500d600768aee4c40
Bradley Mayo <Bradley.Mayo@ibm.com>
2023-03-16 12:54:59 -0400

    Update AdminTask to function properly

--- docker-build/8.5.5.21/scripts/updateKeyStorePassword.py
+++ docker-build/8.5.5.21/scripts/updateKeyStorePassword.py
@@ -9,7 +9,7 @@
 
 def updateKeystorePassword(keyStoreName, keyStorePassword, newPassword):
 
-    AdminTask.changeKeyStorePassword('[-keystoreName ' + keyStoreName + ' -keyStorePassword ' + keyStorePassword + ' -newKeyStorePassword ' + newPassword + ' -newKeyStorePasswordVerify ' + newPassword + ']')
+    AdminTask.changeKeyStorePassword('[-keyStoreName %s -keyStorePassword %s -newKeyStorePassword %s -newKeyStorePasswordVerify %s]' % (keyStoreName, keyStorePassword, newPassword, newPassword))
     AdminConfig.save()
 
-updateKeystorePassword(sys.argv[0], sys.argv[1])	
+updateKeystorePassword(sys.argv[0], sys.argv[1], sys.argv[2])
